fb44f7a13547d8e1e7e4b7e383c6cd99ce0face3
al-uminium <79945949+al-uminium@users.noreply.github.com>
2022-05-21 02:52:43 +0800

    added basic css

--- script.js
+++ script.js
@@ -3,3 +3,39 @@ const board = [
     "", "", "",
     "", "", ""
 ]
+
+const winArrays = [
+    // horizontal
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    // vertical
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    // diagonal
+    [0, 4, 8],
+    [2, 4, 6]
+]
+
+const checkWinner = (board) => {
+    for (const arr of winArrays) {
+        if (board[arr[0]] && board[arr[0]] == board[arr[1]] && board[arr[1]] == board[arr[2]]) {
+            console.log(`Winning array is: ${arr}`)
+        }
+    }
+}
+
+const testCase_1 = [
+    "", "", "",
+    "", "", "",
+    "X", "X", "X"
+]
+
+const testCase_2 = [
+    "", "X", "",
+    "", "X", "",
+    "", "X", ""
+]
+
+checkWinner(testCase_2)
